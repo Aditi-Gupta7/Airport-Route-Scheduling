011cf88e818763fe8a076aa528b7cee102de582c
Aditi Gupta <aditigupta98.ag@gmail.com>
2022-11-13 21:02:29 +0530

    Assign IDs to airport names

--- airport_conn.py
+++ airport_conn.py
@@ -23,9 +23,32 @@ def create_list():
         # Get the starting airport like DLE
         elif line.startswith('Starting Airport'):
             start_airport = line[-3:].strip()
-            
+
+    # print("airports", airports)
+    # print("Routes: ",routes)
+    # print("Start_ airport: " , start_airport)       
     return airports, routes, start_airport
 
+#Airport IDs
+def get_airport_id():
+    for i in range(len(airports)):
+        airport_ids.append([airports[i],i])
+    # print(airport_ids)
+    return airport_ids
 
 
 airports, routes, start_airport = create_list()
+
+airport_ids = []
+get_airport_id()
+
+route_ids = routes.copy()
+
+for i in airport_ids:
+    airport_name = i[0]
+    airport_id = i[1]
+    for i in routes:
+        if routes[0] == airport_name:
+            route_ids[]
+
+
