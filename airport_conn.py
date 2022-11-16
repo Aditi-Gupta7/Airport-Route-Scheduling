d1478e66feb5dd2b604691e593f526b4e1612c2e
Aditi Gupta <aditigupta98.ag@gmail.com>
2022-11-16 20:03:59 +0530

    Final solution

--- airport_conn.py
+++ airport_conn.py
@@ -48,11 +48,11 @@ def get_route_matrix(noof_airports):
         route_matrix[start][dest]  = 1
     return route_matrix
 
-def dfs(i):
+def dfs(i, vis, KR_stack):
     vis[i] = True
     for j in range(length_route_matrix):
         if route_matrix[i][j] == 1 and vis[j] == False:
-            dfs(j)
+            dfs(j, vis, KR_stack)
     KR_stack.append(i)
 
 def get_reverse_route_matrix(noof_airports):
@@ -62,13 +62,33 @@ def get_reverse_route_matrix(noof_airports):
             reverse_route_matrix[i][j] = route_matrix[j][i]
     return reverse_route_matrix
 
-def dfs_and_representative_node(node, representative):
+def dfs_and_representative_node(node, representative, vis, rep, reverse_route_matrix):
     vis[node] = True
     rep[node] = representative
     for j in range(length_route_matrix):
         if reverse_route_matrix[node][j] == 1 and vis[j] == False:
-            dfs_and_representative_node(j, representative)
+            dfs_and_representative_node(j, representative, vis, rep, reverse_route_matrix)
     return rep
+
+def KR_algo(length_route_matrix, rep):
+    vis = [False] * length_route_matrix
+    KR_stack = []
+    # 1. Perform DFS traversal of the graph, and push nodes to stack before back tracking
+    for i in range(length_route_matrix):
+        if vis[i] == False:
+            dfs(i, vis, KR_stack)
+
+    # 2. Change the edge directions of the graph using transpose
+    reverse_route_matrix = get_reverse_route_matrix(noof_airports)
+    # print(reverse_route_matrix)
+
+    # 3. Pop elemens from stack and perform dfs for that element on reverse graph and create a compressed graph
+    vis = [False] * length_route_matrix
+    for i in range(len(KR_stack)-1, -1, -1):
+        node = KR_stack[i]
+        KR_stack.pop(i)
+        if vis[node] == False:
+            dfs_and_representative_node(node, node, vis, rep, reverse_route_matrix)
 
 def initialize_in_degree():
     # Make in-degree of all the rep nodes as 0 and non-rep nodes as -1
@@ -96,41 +116,26 @@ def flights_to_be_added():
             noof_flights = noof_flights + 1
     return noof_flights,destination
 
-# Get list of airports, routes, start_airport
-airports, routes, start_airport = create_list()
+if __name__ == "__main__":
+    airports, routes, start_airport = create_list() #Get list of airports, routes, start_airport
+    noof_airports = len(airports)
+    airport_ids = {} # Assign an ID to each airport
+    get_airport_id(noof_airports)
+    start_airport_id = airport_ids[start_airport]
 
-noof_airports = len(airports)
-# Assign an ID to each airport
-airport_ids = {}
-get_airport_id(noof_airports)
-start_airport_id = airport_ids[start_airport]
+    # Get the adacency matrix for all the routes
+    route_matrix = get_route_matrix(noof_airports)
 
-# Get the adacency matrix for all the routes
-route_matrix = get_route_matrix(noof_airports)
+    # Apply Kosaraju algorith to identify strongly connected components
+    length_route_matrix = len(route_matrix)
+    rep = [-1]*length_route_matrix
+    # vis = [False] * length_route_matrix
+    KR_algo(length_route_matrix, rep)
 
-# Apply Kosaraju algorith to identify strongly connected components
-# 1. Perform DFS traversal of the graph, and push nodes to stack before back tracking
-length_route_matrix = len(route_matrix)
-vis = [False] * length_route_matrix
-KR_stack = []
-for i in range(length_route_matrix):
-    if vis[i] == False:
-        dfs(i)
-# print(KR_stack)
-
-# 2. Change the edge directions of the graph using transpose
-reverse_route_matrix = get_reverse_route_matrix(noof_airports)
-
-# 3. Pop elemens from stack and perform dfs for that element on reverse graph and create a compressed graph
-rep = [-1]*length_route_matrix
-vis = [False] * length_route_matrix
-for i in range(len(KR_stack)-1, -1, -1):
-    node = KR_stack[i]
-    KR_stack.pop(i)
-    if vis[node] == False:
-        dfs_and_representative_node(node, node)
-
-in_deg = initialize_in_degree()
-in_deg = find_in_degree(in_deg)
-noof_flights,destination = flights_to_be_added()
-print(noof_flights, destination)
+    # Find nodes with in-degree = 0 and then find no. of flights to be added
+    in_deg = initialize_in_degree()
+    in_deg = find_in_degree(in_deg)
+    noof_flights,destination = flights_to_be_added()
+    print(noof_flights)
+    for i in destination:
+        print(start_airport,i)
